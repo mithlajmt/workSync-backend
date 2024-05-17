76fc84b8779b16b8fbf3551967aea5b11f032bad
mithlajmatta <144442116+mithlajmatta@users.noreply.github.com>
2024-05-17 11:24:50 +0530

    events completedd

--- app.js
+++ app.js
@@ -42,10 +42,12 @@ db.once('open', () => {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// app.use((req, res, next) => {
-//   console.log('Raw Request Body:', req.body);
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 
 app.use('/', register);
