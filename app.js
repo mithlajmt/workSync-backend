1ba50f57b72a425ccfc811cfdf4fb15ebc0c33bd
mithlajmatta <144442116+mithlajmatta@users.noreply.github.com>
2024-05-17 11:30:59 +0530

    events completeddd

--- app.js
+++ app.js
@@ -42,12 +42,12 @@ db.once('open', () => {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors({
+  origin: 'https://worksync.illuminatespark.com', // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies to be sent
+  optionsSuccessStatus: 204
+}));
 
 
 app.use('/', register);
